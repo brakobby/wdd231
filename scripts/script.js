fedff769cf146b38ad4023ca7462f6c1a0c1b5cc
brakobby <nkobby454@gmail.com>
2025-05-11 02:25:53 +0000

    committed

---
+++ scripts/script.js
@@ -0,0 +1,4 @@
+document.querySelector('.menu-toggle').addEventListener('click', function () {
+    const navLinks = document.querySelector('nav ul');
+    navLinks.classList.toggle('show'); // Toggle the 'show' class
+});
