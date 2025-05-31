dfc89c6a3237098e45bdde807e43985bc976d211
brakobby <nkobby454@gmail.com>
2025-05-31 17:10:51 +0000

    week 02 codded

---
+++ week02/js/scripts.js
@@ -0,0 +1,6 @@
+const hamburgerElement = document.querySelector('#mybutton');
+const navElement = document.querySelector(".nav-links");
+
+hamburgerElement.addEventListener('click', ()=>{
+     
+});
