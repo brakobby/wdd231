b59dc2dcc2d6cf2dca71476b7b48377c993c6601
brakobby <brakobby454@gmail.com>
2025-10-29 09:57:27 +0000

    submitted week1 assignment

--- scripts/course.js
+++ scripts/course.js
@@ -47,7 +47,7 @@ const courses = [
         certificate: 'Web and Computer Programming',
         description: 'This course builds on prior experience in Web Fundamentals and programming. Students will learn to create dynamic websites that use JavaScript to respond to events, update content, and create responsive user experiences.',
         technology: ['HTML', 'CSS', 'JavaScript'],
-        completed: false
+        completed: true
     },
     {
         subject: 'WDD',
